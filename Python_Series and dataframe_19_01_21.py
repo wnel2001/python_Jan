902de5f3140591c88041f6717ec1359b6a5fcc64
wnel2001 <60424113+wnel2001@users.noreply.github.com>
2021-01-19 22:43:18 +0100

    Update Python_Series and dataframe_19_01_21.py

--- Python_Series and dataframe_19_01_21.py
+++ Python_Series and dataframe_19_01_21.py
@@ -1,14 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[27]:
-
-
 import pandas as pd
-
-
-# In[28]:
-
 
 ###Series and DataFrame###
 # Dictionary format data converts to one-dimension  material
@@ -22,16 +12,9 @@ popu=pd.Series(population1)
 print(popu)                         # Key from dictionary format data changed to index of Series
 
 
-# In[29]:
-
-
 # Give low name and column name
 X2=pd.DataFrame([[1, 2, 3], [4, 5, 6], [7, 8, 9]], columns = ['a','b','c'], index=[1,2,3])
 print(X2)
-
-
-# In[30]:
-
 
 ### give 'pop' on the column name of polulation data 
 
@@ -39,9 +22,6 @@ pd.DataFrame(popu, columns = ['pop'])
 
 # one dimension array --> 2 dimension matrix data
 # Can give column name of column vector (열 벡터의 열 이름을 부여 가능)
-
-
-# In[31]:
 
 
 # Example_2
@@ -53,15 +33,9 @@ ind1 = {'Seoul':95,
 print(ind1)
 
 
-# In[32]:
-
-
 # Convert to Series
 ind=pd.Series(ind1)
 print(ind)
-
-
-# In[33]:
 
 
 # Create a DataFrame with the region name index
@@ -69,46 +43,11 @@ y=pd.DataFrame({'pop': popu, 'independent(%)': ind})   # each 1D convert to 2D
 print(y)
 
 
-# In[34]:
-
-
 #materials editing - Slicing
 print(y['independent(%)'])  # Use column name as it is
 
 
-# In[35]:
-
-
 #materials editing - Slicing
 print(y['Busan' : 'Gaungju']) # row from Busan to Gaungu : Slicing Based on Index 
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
 
 
